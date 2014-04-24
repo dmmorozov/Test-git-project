c0a343fe6a4f56425af6f91399a6adb2a3a03593
Dmitrii Morozov <mdmaoff@gmail.com>
2014-04-24 17:47:48 +0400

    Initial Commit

---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Дмитрий Морозов on 24.04.14.
+//  Copyright (c) 2014 Дмитрий Морозов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Дмитрий Морозов on 24.04.14.
+//  Copyright (c) 2014 Дмитрий Морозов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
