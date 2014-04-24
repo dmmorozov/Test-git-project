61799ea69376c06db8a2839830cfc41cf41bcc62
Dmitrii Morozov <mdmaoff@gmail.com>
2014-04-24 19:06:14 +0400

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Дмитрий Морозов on 24.04.14.
+//  Copyright (c) 2014 Дмитрий Морозов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Дмитрий Морозов on 24.04.14.
+//  Copyright (c) 2014 Дмитрий Морозов. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
